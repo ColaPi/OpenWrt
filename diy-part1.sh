558aa8f6c4adaa2c4af0ff449d636be7019ad127
colachg <colachg@gmail.com>
2022-06-07 19:19:29 +0800

    Fix relative path error.

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,10 +3,8 @@
 sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
 
 # add luci-theme-argon
-cd openwrt/package/lean
-rm -rf luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+rm -rf ./package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/
 
 # add luci-app-kcptun
-cd openwrt/package/lean
-git clone https://github.com/kuoruan/luci-app-kcptun.git
+git clone https://github.com/kuoruan/luci-app-kcptun.git ./package/lean/
