59908eff13a9fa0d012734194a88dc5992f7b0e9
colachg <colachg@gmail.com>
2022-06-07 21:51:39 +0800

    Fix relative path error.

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,6 +5,3 @@ sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_gener
 # add luci-theme-argon
 rm -rf ./package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon
-
-# add luci-app-kcptun
-git clone https://github.com/kuoruan/luci-app-kcptun.git ./package/lean/luci-app-kcptun
