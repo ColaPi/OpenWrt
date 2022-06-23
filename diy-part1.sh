e8174560ad738684f478f427a16f789c6c126dfd
Cola Chen <colachg@gmail.com>
2022-06-23 06:47:36 +0000

    Remove custom luci-theme-argon

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,5 +3,5 @@
 sed -i 's/192.168.1.1/192.168.199.1/g' package/base-files/files/bin/config_generate
 
 # add luci-theme-argon
-rm -rf ./package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon
+# rm -rf ./package/lean/luci-theme-argon
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./package/lean/luci-theme-argon
